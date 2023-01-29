74f95257e2a41a1f0628d8d31fa1834d1ccf5372
rmukaila <abdulrashid281@gmail.com>
2023-01-29 10:08:05 +0300

    added search functionality

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ job_ads_backend
 2)Write a code to add the kaggle data to the mongo database
 3)add filtering feature. Use basic filtering for start like just country and jobe type (remote/onsite)
 4)Add pagination
-5)Add celery and RabitMQ
+5)[[[Add celery and RabitMQ]] no need for this in this application
+6) Add testing beginning with the fronend testing followed by the backend testing
--- jobsdb.py
+++ jobsdb.py
@@ -7,8 +7,13 @@ mongoClNT = MongoClient(mongo_url)
 JOB_DB = mongoClNT['jobs_advertisements_db']
 job_col = JOB_DB['job_ads']
 # mydict = { "name": "John", "address": "Highway 37" }
+# mock_data = [{'id':5,'title':"Data engineer",'tags':['mid','python','AWS','GCP'],'description':"Must have GCP certification, etc"},
+#     {'id':6,'title':"Data scientist",'tags':['mid','python','R'],'description':"Must have GCP certification"},
+#     {'id':7,'title':"python developer",'tags':['mid','python','docker'],'description':"Must have GCP certification"},
+#     {'id':8,'title':"full-stack developer",'tags':['mid','python','react'],'description':"Must have GCP certification"}
+# ]
 
-# job_col.insert(mydict)
+# job_col.insert(mock_data)
 
 # print(JOB_DB.list_collection_names())
 
