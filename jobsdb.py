373f65a48a38aac7fe3122e1b09a60bfc02c1b97
rmukaila <abdulrashid281@gmail.com>
2023-02-16 09:57:51 +0300

    added dnspython==2.3.0 to the requirements.txt file

--- jobsdb.py
+++ jobsdb.py
@@ -10,10 +10,10 @@ mongoClNT = MongoClient(mongo_url)
 JOB_DB = mongoClNT['jobs_advertisements_db']
 job_col = JOB_DB['job_ads']
 # mydict = { "name": "John", "address": "Highway 37" }
-# mock_data = [{'id':5,'title':"Data engineer",'tags':['mid','python','AWS','GCP'],'description':"Must have GCP certification, etc"},
-#     {'id':6,'title':"Data scientist",'tags':['mid','python','R'],'description':"Must have GCP certification"},
-#     {'id':7,'title':"python developer",'tags':['mid','python','docker'],'description':"Must have GCP certification"},
-#     {'id':8,'title':"full-stack developer",'tags':['mid','python','react'],'description':"Must have GCP certification"}
+# mock_data = [{'id':1,'title':"Data engineer",'tags':['mid','python','AWS','GCP'],'description':"Must have GCP certification, etc"},
+#     {'id':2,'title':"Data scientist",'tags':['mid','python','R'],'description':"Must have GCP certification"},
+#     {'id':3,'title':"python developer",'tags':['mid','python','docker'],'description':"Must have GCP certification"},
+#     {'id':4,'title':"full-stack developer",'tags':['mid','python','react'],'description':"Must have GCP certification"}
 # ]
 
 # job_col.insert(mock_data)
